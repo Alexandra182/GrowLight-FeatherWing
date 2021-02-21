24cf1e17b9d5e8c6824be7bd17864adaae307d9b
alexandra <alexandra.covor@gmail.com>
2021-02-21 22:37:07 +0200

    timer code

--- Arduino/adafruitio/adafruitio.ino
+++ Arduino/adafruitio/adafruitio.ino
@@ -18,11 +18,18 @@
 #define redPin  16
 #define bluePin 15
 
+#define startHour 8 // when should the light turn on
+
 /************************ Example Starts Here *******************************/
 
-// set up the 'blue' feed
-AdafruitIO_Feed *blue = io.feed("Blue");
-AdafruitIO_Feed *red = io.feed("Red");
+// set up the feeds
+AdafruitIO_Feed *blue   = io.feed("Blue");
+AdafruitIO_Feed *red    = io.feed("Red");
+AdafruitIO_Feed *ontime = io.feed("On Time");
+AdafruitIO_Time *iso    = io.time(AIO_TIME_ISO);
+
+// variables
+int redIntensity, blueIntensity, onTimeValue;
 
 void setup() {
   // initialise LEDs pins as outputs
@@ -43,12 +50,14 @@ void setup() {
   // start MQTT connection to io.adafruit.com
   io.connect();
 
-  // set up a message handler for the blue 7 red feeds.
+  // set up message handlers for the feeds.
   // the handleMessage function (defined below)
   // will be called whenever a message is
   // received from adafruit io.
   blue->onMessage(handleBlue);
   red->onMessage(handleRed);
+  iso->onMessage(handleISO);
+  ontime->onMessage(handleOnTime);
 
   // wait for an MQTT connection
   // NOTE: when blending the HTTP and MQTT API, always use the mqttStatus
@@ -63,6 +72,7 @@ void setup() {
   // this MQTT client after the io client is connected.
   blue->get();
   red->get();
+  ontime->get();
 
   // we are connected
   Serial.println();
@@ -81,9 +91,6 @@ void loop() {
   // a delay() in the main program loop.
 }
 
-// this function is called whenever a 'blue'/'red' message
-// is received from Adafruit IO. it was attached to
-// the counter feed in the setup() function above.
 void handleRed(AdafruitIO_Data *data) {
 
   // print out the received light value
@@ -91,11 +98,10 @@ void handleRed(AdafruitIO_Data *data) {
   String received = data->value();
   int redValue = received.toInt();
   Serial.print(redValue);
-  int mapped = map(redValue, 0, 100, 0, 700);
+  redIntensity = map(redValue, 0, 100, 0, 700);
   Serial.print(" mapped: ");
-  Serial.println(mapped);
-  analogWrite(redPin, mapped);
-  
+  Serial.println(redIntensity);
+
 }
 
 void handleBlue(AdafruitIO_Data *data) {
@@ -105,9 +111,36 @@ void handleBlue(AdafruitIO_Data *data) {
   String received = data->value();
   int blueValue = received.toInt();
   Serial.print(blueValue);
-  int mapped = map(blueValue, 0, 100, 0, 700);
+  blueIntensity = map(blueValue, 0, 100, 0, 700);
   Serial.print(" mapped: ");
-  Serial.println(mapped);
-  analogWrite(bluePin, mapped);
-  
+  Serial.println(blueIntensity);
+
+}
+
+void handleOnTime(AdafruitIO_Data *data) {
+
+  // print out the received light value
+  Serial.print("received <- ontime ");
+  String received = data->value();
+  onTimeValue = received.toInt();
+  Serial.print(onTimeValue);
+
+}
+
+void handleISO(char *data, uint16_t len) {
+
+  Serial.print("ISO Feed: ");
+  String strdata = data;
+  String h = strdata.substring(11, 13);
+  int hour = h.toInt() + 2;
+  Serial.println(hour);
+
+  if ((hour >= startHour) && hour <= (startHour + onTimeValue)) {
+    analogWrite(bluePin, blueIntensity);
+    analogWrite(redPin, redIntensity);
+  } else {
+    analogWrite(bluePin, 0);
+    analogWrite(redPin, 0);
+  }
+
 }
